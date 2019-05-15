e3a751112f20b5fc9b6fd56e0e39edc1e307422f
Alon Cohen <alon@cohencodes.com>
2019-05-14 20:09:39 -0700

    started tests for POST and DELETE

--- src/bookmarks/bookmark-router.js
+++ src/bookmarks/bookmark-router.js
@@ -1,86 +1,79 @@
-const express = require('express')
-const uuid = require('uuid/v4')
-const logger = require('../logger')
-const bookmarks = require('../store')
-const BookmarkService = require('./bookmarks-service')
+const express = require('express');
+const uuid = require('uuid/v4');
+const logger = require('../logger');
+const bookmarks = require('../store');
+const BookmarkService = require('./bookmarks-service');
 
-const bookmarksRouter = express.Router()
-const bodyParser = express.json()
-
-bookmarksRouter
-    .route('/bookmarks')
-    .get(bodyParser, (req, res) => {
-        const knexInstance = req.app.get('db')
-        BookmarkService.getAllBookmarks(knexInstance)
-            .then(bookmarks => {
-                res.json(bookmarks);
-            })
-    })
-    .post(bodyParser, (req, res) => {
-        const { name, url } = req.body
-
-        let id = uuid();
-        let newBookmark = {
-            id,
-            name,
-            url
-        }
-
-        if(!name) {
-            logger.error(`Name cannot be blank`)
-            return res
-            .status(400)
-            .json({ "error": "Name cannot be blank" })
-        }
-
-        if(!url) {
-            logger.error(`URL cannot be blank`)
-            return res
-            .status(400)
-            .json({ "error": "URL cannot be blank" })
-        }
-        
-        bookmarks.push(newBookmark)
-
-        return res
-            .status(201)
-            .location(`http://localhost:8000/bookmarks/${id}`)
-            .json(bookmarks)
-    })
+const bookmarksRouter = express.Router();
+const bodyParser = express.json();
 
 bookmarksRouter
-    .route('/bookmarks/:id')
-    .get(bodyParser, (req, res, next) => {
-        let id = req.params.id;        
-        const knexInstance = req.app.get('db')
+  .route('/bookmarks')
+  .get(bodyParser, (req, res, next) => {
+    const knexInstance = req.app.get('db');
+    BookmarkService.getAllBookmarks(knexInstance)
+      .then(bookmarks => {
+        res.json(bookmarks);
+      })
+      .catch(next);
+  })
+  .post(bodyParser, (req, res, next) => {
+    const knexInstance = req.app.get('db');
+    const { name, url, rating } = req.body;
 
-        BookmarkService.getById(knexInstance, id)
-            .then(result => {
-                if (!result) {
-                    res.status(404).json({
-                        error: { message: 'No bookmark at that ID' }
-                    })
-                }
-                res.status(200).json(result)
-            })
-            .catch(next)
-    })
-    .delete(bodyParser, (req, res) => {
-        let id = req.params.id;
-        let bookmarkIndex = bookmarks.findIndex(bookmark => bookmark.id === id)
-      
-        if (bookmarkIndex === -1) {
-          logger.error(`Bookmark not found`)
-          return res
-            .status(404)
-            .json({ "error": "Bookmark not found" })
+    let id = uuid();
+    let newBookmark = {
+      id,
+      name,
+      url,
+      rating
+    };
+
+    const requiredKeys = [name, url, rating];
+
+    requiredKeys.forEach((key, value) => {
+      if (key[value] === null) {
+        return res.status(400).json({ error: `${key} cannot be blank` });
+      }
+    });
+
+    BookmarkService.addBookmark(knexInstance, newBookmark).then(bookmark => {
+      return res
+        .status(201)
+        .location(`http://localhost:8000/bookmarks/${bookmark.id}`)
+        .json(bookmarks);
+    });
+  });
+
+bookmarksRouter
+  .route('/bookmarks/:id')
+  .get(bodyParser, (req, res, next) => {
+    let id = req.params.id;
+    const knexInstance = req.app.get('db');
+
+    BookmarkService.getById(knexInstance, id)
+      .then(result => {
+        if (!result) {
+          res.status(404).json({
+            error: { message: 'No bookmark at that ID' }
+          });
         }
-      
-        bookmarks.splice(bookmarkIndex, 1)
-      
-        return res
-          .status(200)
-          .end()
-    })
+        res.status(200).json(result);
+      })
+      .catch(next);
+  })
+  .delete(bodyParser, (req, res) => {
+    let id = req.params.id;
+    let bookmarkIndex = bookmarks.findIndex(bookmark => bookmark.id === id);
 
-module.exports = bookmarksRouter
+    if (bookmarkIndex === -1) {
+      logger.error(`Bookmark not found`);
+      return res.status(404).json({ error: 'Bookmark not found' });
+    }
+
+    bookmarks.splice(bookmarkIndex, 1);
+
+    return res.status(200).end();
+  });
+
+module.exports = bookmarksRouter;
--- test/bookmarks-service.spec.js
+++ test/bookmarks-service.spec.js
@@ -1,95 +1,118 @@
-const knex = require('knex')
+const knex = require('knex');
 const app = require('../src/app');
 const { MakeBookmarksArray } = require('./bookmarks.fixtures');
 
-describe.only('Bookmarks Service', () => {
-    let db
+describe('Bookmarks Service', () => {
+  let db;
 
   const testBookmarks = MakeBookmarksArray();
 
   before('make knex instance', () => {
     db = knex({
       client: 'pg',
-      connection: process.env.TEST_DB_URL,
-    })
+      connection: process.env.TEST_DB_URL
+    });
     app.set('db', db);
-  })
+  });
 
-  before('clean the table', () => db('bookmarks').truncate())
-  after('disconnect from db', () => db.destroy())
-  afterEach('cleanup', () => db('bookmarks').truncate())
+  before('clean the table', () => db('bookmarks').truncate());
+  after('disconnect from db', () => db.destroy());
+  afterEach('cleanup', () => db('bookmarks').truncate());
 
   describe(`GET /bookmarks`, () => {
     context(`has bookmarks`, () => {
-        beforeEach(() => {
-            return db.into('bookmarks').insert(testBookmarks);
-        });
+      beforeEach(() => {
+        return db.into('bookmarks').insert(testBookmarks);
+      });
 
-        it('responds with 200 and has all bookmarks', () => {
-            return supertest(app)
-              .get('/bookmarks')
-              .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
-              .expect(200, testBookmarks);
-          })
-    })
+      it('responds with 200 and has all bookmarks', () => {
+        return supertest(app)
+          .get('/bookmarks')
+          .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
+          .expect(200, testBookmarks);
+      });
+    });
 
-    context(`has no bookmarks`, () => {      
+    context(`has no bookmarks`, () => {
       it('responds with 200 and has empty list', () => {
         return supertest(app)
           .get('/bookmarks')
           .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
-          .expect(200, [])
-      })
-      })
-  })
+          .expect(200, []);
+      });
+    });
+  });
 
   describe(`GET /bookmarks/:id`, () => {
     context(`has bookmarks`, () => {
-        beforeEach(() => {
-            return db.into('bookmarks').insert(testBookmarks);
-        });
+      beforeEach(() => {
+        return db.into('bookmarks').insert(testBookmarks);
+      });
 
-        it('returns bookmark with given id', () => {
-          let id = 3;
-          let expectedBookmark = testBookmarks.filter(bookmark => bookmark.id === id);
-          return supertest(app)
-            .get(`/bookmarks/${id}`)
-            .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
-            .expect(200, expectedBookmark[0])
-        })
-
-        it('returns error if no bookmark at given id', () => {
-            let id = 10;
-            let expectedError = {
-                error: { message: 'No bookmark at that ID' }
-            };
-            return supertest(app)
-                .get(`/bookmarks/${id}`)
-                .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
-                .expect(404, expectedError)
-        })
-    })
-
-    })
-
-    describe(`Unauthorized requests`, () => {
-        beforeEach(() => {
-            return db.into('bookmarks').insert(testBookmarks);
-        });
-
-        it('responds with 401 unauthorized request to /bookmarks', () => {
-            const expectedError = { error: 'Unauthorized request' }
-          return supertest(app)
-            .get(`/bookmarks`)
-            .expect(401, expectedError)
-        })
-
-        it('responds with 401 unauthorized request to /bookmarks/:id', () => {
-          const bookmarkId = testBookmarks[0].id;
-          const expectedError = { error: 'Unauthorized request' }
+      it('returns bookmark with given id', () => {
+        let id = 3;
+        let expectedBookmark = testBookmarks.filter(
+          bookmark => bookmark.id === id
+        );
         return supertest(app)
-          .get(`/bookmarks/${bookmarkId}`)
-          .expect(401, expectedError)
-      })
-    })
-})
+          .get(`/bookmarks/${id}`)
+          .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
+          .expect(200, expectedBookmark[0]);
+      });
+
+      it('returns error if no bookmark at given id', () => {
+        let id = 10;
+        let expectedError = {
+          error: { message: 'No bookmark at that ID' }
+        };
+        return supertest(app)
+          .get(`/bookmarks/${id}`)
+          .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
+          .expect(404, expectedError);
+      });
+    });
+  });
+
+  describe(`Unauthorized requests`, () => {
+    beforeEach(() => {
+      return db.into('bookmarks').insert(testBookmarks);
+    });
+
+    it('responds with 401 unauthorized request to /bookmarks', () => {
+      const expectedError = { error: 'Unauthorized request' };
+      return supertest(app)
+        .get(`/bookmarks`)
+        .expect(401, expectedError);
+    });
+
+    it('responds with 401 unauthorized request to /bookmarks/:id', () => {
+      const bookmarkId = testBookmarks[0].id;
+      const expectedError = { error: 'Unauthorized request' };
+      return supertest(app)
+        .get(`/bookmarks/${bookmarkId}`)
+        .expect(401, expectedError);
+    });
+  });
+
+  describe.only('POST /bookmarks', () => {
+    it('creates a bookmark and returns the created bookmark', () => {
+      const newBookmark = {
+        name: 'New test bookmark',
+        url: 'https://www.newbookmark.com',
+        rating: '4'
+      };
+
+      return supertest(app)
+        .post('/bookmarks')
+        .send(newBookmark)
+        .expect(201)
+        .then(res => {
+          expect(res.body).to.eql(newBookmark);
+          expect(res.body.name).to.eql(newBookmark.name);
+          expect(res.body.url).to.eql(newBookmark.url);
+          expect(res.body.rating).to.eql(newBookmark.rating);
+          expect(res.body).to.have.property('id');
+        });
+    });
+  });
+});
